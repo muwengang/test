ad79dce2e0cf02d0d4779c689a985e0302ef7e4f
mwg <1249325832@qq.com>
2017-11-19 14:04:16 +0800

    add strstr.c

---
+++ strstr.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int my_strlen(const char *src){
+	if(src == NULL || *src == '\0')
+		return 0;
+	return 1+my_strlen(++src);
+}
+char *my_strstr(char *src, char *search){
+	if(src == NULL || search == NULL)
+		return NULL;
+	char *temp_src = src;
+	char *temp_search = search;
+	int len = my_strlen(search);
+	int len1 = my_strlen(src);
+	printf("len = %d,len2= %d",len, len1);
+	int i = 0, j = 0;
+	while(temp_src != '\0' && j < len1){
+		if(*temp_src == *temp_search){
+			while(i < len){
+				if(temp_src[i] != temp_search[i]){
+					printf("i = %d\n",i);
+					i = 0;
+					break;
+				}
+				i++;
+				
+					printf("i = %d\n",i);
+			}
+					printf("j = %d\n",j);
+			if(i != 0)
+				return temp_src;
+		}
+		++j;
+		temp_src++;
+	}
+	printf("%s", temp_src);
+	return NULL;
+}
+int main(int argc, char **argv){
+	char *buf  = "hello world,are you ok?";
+	char *result = my_strstr(buf, "word");
+	if(result)
+		printf("%s\n", result);
+	return 0;
+}
